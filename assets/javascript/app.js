b11ed9d90c9815266f9c3f4f4adb6134654d6e4d
Jordan Boggs <boggsjp@gmail.com>
2017-12-03 17:13:08 -0700

    built first stages of countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,46 @@ $("#start").click(function() {
   $("#gameboard").show();
 
   // Let's start the show
-  startGame();
+  game.start();
 });
 
-function startGame() {
-  
-}
+var game = {
+  time: 30,
+
+  start: function() {
+    intervalId = setInterval(game.count, 1000);
+    clockRunning = true;
+  },
+
+  count: function() {
+    // Decrement time by 1
+    game.time--;
+
+    // Get the current time and format it
+    let currentTime = game.timeConverter(game.time);
+
+    // Display time in #timer
+    $("#timer").text(currentTime);
+  },
+
+  timeConverter: function(t) {
+    // Takes the current time in seconds and converts it to
+    // minutes and seconds (mm:ss)
+    let minutes = Math.floor(t / 60);
+    let seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+};
