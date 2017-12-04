065b8401f4d3e59c96a1158f270c8ee3ef7e947b
Jordan Boggs <boggsjp@gmail.com>
2017-12-03 17:03:45 -0700

    structure for timer question and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
+/*
+ * S T A R   T R E K   T R I V I A
+ * Jordan Boggs - Dec 3 2017 
+ */
+
 $("#start").click(function() {
   $("#start-screen").hide();
   $("#gameboard").show();
+
+  // Let's start the show
+  startGame();
 });
+
+function startGame() {
+  
+}
